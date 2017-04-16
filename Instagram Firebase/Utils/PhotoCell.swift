21ec7dd4a09ebc4f01e8549127c388b8e52666d7
Vyacheslav Nagornyak <nagornyak.slava@gmail.com>
2017-04-16 22:47:56 +0300

    Added PhotoCell

---
+++ Instagram Firebase/Utils/PhotoCell.swift
@@ -0,0 +1,32 @@
+//
+//  PhotoCell.swift
+//  Instagram Firebase
+//
+//  Created by Vyacheslav Nagornyak on 4/16/17.
+//  Copyright © 2017 Vyacheslav Nagornyak. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+  
+  // MARK: - Variables
+  let imageView: UIImageView = {
+    let iv = UIImageView()
+    iv.contentMode = .scaleAspectFill
+    iv.clipsToBounds = true
+    return iv
+  }()
+  
+  // MARK: - Initializers
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    
+    addSubview(imageView)
+    imageView.anchor(top: topAnchor, leading: leadingAnchor, trailing: trailingAnchor, bottom: bottomAnchor)
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
